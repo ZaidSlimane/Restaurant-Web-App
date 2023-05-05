7869997235773c13e51ee7dd8cda71a4fb2bbb26
Zaid Slimane <zaidslimane20@gmail.com>
2023-05-05 14:45:09 +0100

    Merged

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function () {
+    let header = this.document.getElementById("Home");
+    header.classList.toggle("fixed", this.window.scrollY > 0)
+})
+let burgerBtn = document.querySelector(".burger i");
+burgerBtn.onclick = function () {
+    burgerBtn.classList.toggle("fa-xmark");
+    let menu = document.querySelector(".header nav ul")
+    menu.classList.toggle("active");
+}
